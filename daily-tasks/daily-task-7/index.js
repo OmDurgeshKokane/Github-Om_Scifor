79e0edf87be70e383319b704ab996630bcd533f2
Om Kokane <om.kokane12345@gmail.com>
2024-10-29 20:42:29 +0530

    Adding tests 7

---
+++ daily-tasks/daily-task-7/index.js
@@ -0,0 +1,21 @@
+let number = 1;
+console.log(typeof(number));
+
+let name = "Om kokane"
+console.log(typeof(name))
+
+let boolean = true;
+console.log(typeof(boolean));
+
+let val = null;
+console.log(typeof(val));
+
+let obj = {
+    name: "Om kokane",
+    age: 20,
+
+};
+console.log(typeof(obj));
+
+let val2 = undefined;
+console.log(typeof(val2));
