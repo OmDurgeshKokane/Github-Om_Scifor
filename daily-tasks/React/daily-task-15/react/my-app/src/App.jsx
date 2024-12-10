50575e12aa963b782e22d739e56d45cc6344518b
Om Kokane <om.kokane12345@gmail.com>
2024-12-10 21:03:14 +0530

    add react in react folder task-15

---
+++ daily-tasks/React/daily-task-15/react/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div>
+      <h1>This is the Heading</h1>
+      <p>This is a paragraph describing the heading above. React makes it easy to build UI components!</p>
+    </div>
+  );
+}
+
+export default App
