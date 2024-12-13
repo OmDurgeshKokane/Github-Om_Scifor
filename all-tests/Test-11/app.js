f662cb853978135420b79ef1e58b1a7c79b5349e
Om Kokane <om.kokane12345@gmail.com>
2024-12-13 21:05:17 +0530

    add test-11

--- all-tests/Test-11/app.js
+++ all-tests/Test-11/app.js
@@ -1,18 +1,27 @@
+let div1 = document.querySelector("#div1");
+let div2 = document.querySelector("#div2");
+let div3 = document.querySelector("#div3");
+
 
 let manupulateArray = async (array) =>{
     let newArr =  await array;
     console.log(newArr);
+    div1.append(newArr);
     newArr.push("Volvo");
     console.log(newArr);
+    div2.append(newArr);
     arrayUppercase(newArr);
 }
 
-let arrayUppercase =  (array) =>{
+let arrayUppercase = (array) => {
     array.forEach(element => {
-        let uppercase = element.toUpperCase();
-        console.log(uppercase);
+        let uppercase = element.toUpperCase(); 
+        let uppercaseWithComma = uppercase.split(" ").join(", "); 
+        let paragraph = document.createElement("p"); 
+        paragraph.textContent = uppercaseWithComma; 
+        div3.append(paragraph); 
     });
-}
+};
 
 let input = prompt("Enter the array elements by comma seprated");
 let array = input.split(",");
