8abee715b9a3c40f473b05ec7b20a852c69286b1
Om Kokane <om.kokane12345@gmail.com>
2025-01-16 19:12:15 +0530

    Added

--- all-tests/New Project/clone-website/HTML-CSS-JS/script/script.js
+++ all-tests/New Project/clone-website/HTML-CSS-JS/script/script.js
@@ -5,3 +5,18 @@ function scrollTestimonials(direction) {
   container.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
 }
 
+
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    scrollTopBtn.style.display = 'block';
+  } else {
+    scrollTopBtn.style.display = 'none';
+  }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
